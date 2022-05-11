7213e42e4c69d0365e134ee7161258da7e70d747
José Luan Lima da Costa <joseluan620@gmail.com>
2022-05-11 10:12:12 -0300

    first commit

---
+++ src/test/java/br/com/clinicaback/ClinicaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.clinicaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
